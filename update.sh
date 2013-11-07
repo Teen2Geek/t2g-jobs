f7aa1e5a07d43ea1f324cc4cd06c6f972e6a6dd1
Krish Dholakiya <krishna.dholakiya@gmail.com>
2013-11-06 18:23:06 -0700

    Initial Upload

---
+++ update.sh
@@ -0,0 +1,8 @@
+echo "Compiling Assets....";
+harp compile _harp/ .;
+echo "Assets compiled!";
+echo "Pushing to Github Pages...";
+git add .;
+git commit -m "$1";
+git push origin gh-pages;
+"Pushed - $1";
