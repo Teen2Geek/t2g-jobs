f1a3ae3da5b332a11524b1dad200a4c9a8ba5610
Krish Dholakiya <krishna.dholakiya@gmail.com>
2013-11-07 19:07:49 -0700

    Added job search

--- _harp/main.js
+++ _harp/main.js
@@ -2,6 +2,6 @@ var app = angular.module('jobs', []);
 
 function internshipCtrl($scope, $http) {
 	$http({method: 'GET', url: '/jobs.json'}).success(function(data){
-		$scope.jobs = data.jobs;
+		$scope.jobs = data;
 	});
 }
