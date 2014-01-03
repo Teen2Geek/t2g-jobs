6b944f384d7d17bce9bb65e237a6c71e1fad84ed
Krish Dholakiya <krishna.dholakiya@gmail.com>
2014-01-03 13:50:40 -0700

    New Job - Flicker Labs

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = angular.module('jobs', []);
 function internshipCtrl($scope, $http) {
 	$http({method: 'GET', url: '/jobs.json'}).success(function(data){
 		$scope.jobs = data.jobs;
+		$scope.mail = 'mailto:' + job.contact + '?subject=' + job.name + '%20Inquiry%20-%20T2G';
 	});
 
 }
